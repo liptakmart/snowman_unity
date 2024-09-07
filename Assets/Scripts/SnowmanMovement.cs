5fc0b4768006d7d19f2ff0ccd0868832217815db
liptakmart <liptakmart@gmail.com>
2024-09-07 22:35:15 +0200

    movement redone

--- Assets/Scripts/SnowmanMovement.cs
+++ Assets/Scripts/SnowmanMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,15 @@ public class SnowmanMovement : MonoBehaviour
 {
     public float maxSpeed = 5f; // Maximum speed the character can reach
     public float acceleration = 2f; // How fast the character accelerates
-    private float currentSpeed = 0f; // Current speed of the character
-    private Vector3 movement; // Movement direction
     public GameObject snowmanModel;
 
+    private float currentSpeed = 0f; // Current speed of the character
+    private Vector3 movement; // Movement direction
+
+    private void Start()
+    {
+        
+    }
     void Update()
     {
         // Move the character
@@ -32,54 +38,59 @@ public class SnowmanMovement : MonoBehaviour
             currentSpeed += acceleration * Time.deltaTime;
             // Clamp the speed to the maximum speed
             currentSpeed = Mathf.Clamp(currentSpeed, 0f, maxSpeed);
+            // Move the character in the direction of the movement vector
+            transform.Translate(movement * currentSpeed * Time.deltaTime, Space.World);
+
+            TurnModel(new Vector2(moveHorizontal, moveVertical));
         }
         else
         {
             // Optionally, decrease speed gradually when there is no input (optional smooth deceleration)
             currentSpeed = 0f;
         }
-
-        // Move the character in the direction of the movement vector
-        transform.Translate(movement * currentSpeed * Time.deltaTime, Space.World);
-        TurnModel(moveHorizontal, moveVertical);
     }
 
     /// <summary>
-    /// Turns model accordingly to movement direction
+    /// Turns model accordingly to movement direction.
+    /// X horizontal, Y vertical
     /// </summary>
-    /// <param name="moveHorizontal"></param>
-    /// <param name="moveVertical"></param>
-    void TurnModel(float moveHorizontal, float moveVertical)
+    /// <param name="vec"></param>
+    void TurnModel(Vector2 vec)
     {
-        if (moveVertical > 0 && moveHorizontal > 0)
+        if (movement.magnitude == 0)
+        {
+            return;
+        }
+
+        if (vec.y > 0 && vec.x > 0)
         {
             snowmanModel.transform.rotation = Quaternion.Euler(new Vector3(0, 135, 0));
         }
-        else if (moveVertical > 0 && moveHorizontal < 0)
+        else if (vec.y > 0 && vec.x < 0)
         {
             snowmanModel.transform.rotation = Quaternion.Euler(new Vector3(0, 45, 0));
         }
-        else if (moveVertical < 0 && moveHorizontal > 0)
+        else if (vec.y < 0 && vec.x > 0)
         {
             snowmanModel.transform.rotation = Quaternion.Euler(new Vector3(0, 225, 0));
         }
-        else if (moveVertical < 0 && moveHorizontal < 0)
+        else if (vec.y < 0 && vec.x < 0)
         {
             snowmanModel.transform.rotation = Quaternion.Euler(new Vector3(0, 315, 0));
         }
-        else if (moveVertical > 0)
+        else if (vec.y > 0)
         {
             snowmanModel.transform.rotation = Quaternion.Euler(new Vector3(0, 90, 0));
         }
-        else if (moveVertical < 0)
+        else if (vec.y < 0)
         {
             snowmanModel.transform.rotation = Quaternion.Euler(new Vector3(0, 270, 0));
         }
-        else if (moveHorizontal > 0)
+        else if (vec.x > 0)
         {
             snowmanModel.transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
         }
-        else if (moveHorizontal < 0)
+        else if (vec.x < 0)
         {
             snowmanModel.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
         }
